6e73c33d0a265cbf4933c990ce86f4cc7f56189b
stasrdv <stasrdv@gmail.com>
2018-11-19 17:57:26 +0200

    bug fixed

--- src/components/about.js
+++ src/components/about.js
@@ -20,7 +20,8 @@ export class AboutPage extends React.Component {
           <hr className="my-4" />
           <p className="lead">
             {" "}
-            My passionate hobby is Cooking<span className="fa fa-cutlery" />
+            My passionate hobby is Cooking
+            <span className="fa fa-cutlery" />
           </p>
           <img
             src="https://scontent.fsdv1-1.fna.fbcdn.net/v/t1.0-9/36062955_1988795404472686_6474415611035254784_n.jpg?_nc_cat=104&_nc_ht=scontent.fsdv1-1.fna&oh=39645021ccd662bea8f8f864981b5d5d&oe=5CB08044"
